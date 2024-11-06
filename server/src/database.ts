2215357581c052ae60fcd202d2b0d0a43297e5be
Keith Sialana <ninjabasketball@hotmail.com>
2024-11-06 14:04:49 -0500

    updated database stuff again

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
-dotenv.config({path:'env/.env'});
+dotenv.config({ path: "env/.env" });
 import { Sequelize } from "sequelize";
 
 const env = process.env;
 
-export const seq = env.DB_URL
+const sequelize = env.DB_URL
 	? new Sequelize(env.DB_URL)
 	: new Sequelize({
 			host: "localhost",
@@ -16,3 +16,5 @@ export const seq = env.DB_URL
 			password: env.DB_PASS || "rootroot",
 			database: env.DB_DATABASE,
 	  });
+
+export default sequelize;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
+import sequelize from './database';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +11,9 @@ app.use(express.json());
 // Use the user routes
 app.use('/api', userRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+sequelize.sync({force:true}).then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
 });
+
